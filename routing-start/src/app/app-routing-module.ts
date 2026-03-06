f270bbc342db2d22e0b8163771aa59c9b6f70479
Lucas Landivar <landivarlucas67@gmail.com>
2026-03-05 21:10:41 -0300

    .

--- routing-start/src/app/app-routing-module.ts
+++ routing-start/src/app/app-routing-module.ts
@@ -37,7 +37,7 @@ const appRoutes: Routes = [
       },
     ],
   },
-  // { path: 'not-found', component: PageNotFoundComponent },
+  // ?{ path: 'not-found', component: PageNotFoundComponent },
   {
     path: 'not-found',
     component: ErrorPageComponent,
